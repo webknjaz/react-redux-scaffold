9b8cecba463e737509f85055bb04c4b6e7ff620c
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2016-03-23 21:44:21 +0200

    Add map dispatch to props

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,21 +4,9 @@ import { connect } from 'react-redux';
 import { loadBooksSuccess, loadBooksFailure, loadBooksRequest } from '../actions/TodosAction.js';
 
 class BookList extends Component {
-  static fetchData(dispatch) {
-    dispatch(loadBooksRequest());
-    return fetch('/api/books')
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json);
-          return json;
-        }).then(
-          (books) => dispatch(loadBooksSuccess(books)),
-          (error) => dispatch(loadBooksFailure(error))
-        );
-  }
 
   componentDidMount() {
-    this.constructor.fetchData((_) => _);
+    this.props.fetchData();
   }
 
   render() {
@@ -33,13 +21,31 @@ class BookList extends Component {
     return (
 
       <section className="book-list">
-        {books[0]}
         Book List here
+        {books}
       </section>
     );
   }
 }
 
-export default connect(state => ({
-  books: state.books
-}))(BookList);
+
+const mapStateToProps = (state) => ({
+  books: state.todos // TODO: pass books here somehow
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchPosts: () => {
+    dispatch(loadBooksRequest());
+    fetch('/api/books')
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          return json;
+        }).then(
+          (books) => dispatch(loadBooksSuccess(books)),
+          (error) => dispatch(loadBooksFailure(error))
+        );
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
